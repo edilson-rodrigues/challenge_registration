4ccb6be863b9f5b07d77b75a976d9b217d4623e8
Edilson Rodrigues <edarodrigs@gmail.com>
2021-05-07 19:22:10 -0300

    feat: main app project with modular 3.0

---
+++ lib/app/app_module.dart
@@ -0,0 +1,22 @@
+import 'package:flutter_modular/flutter_modular.dart';
+
+import 'package:challenge_registration/app/modules/login/login_module.dart';
+import 'package:challenge_registration/app/shared/services/local_storage_service/local_storage_service.dart';
+import 'package:challenge_registration/app/shared/shared.dart';
+
+import 'modules/home/home_module.dart';
+import 'modules/splash/splash_module.dart';
+
+class AppModule extends Module {
+  @override
+  final List<Bind> binds = [
+    Bind<ILocalStorage>((i) => SharedLocalStorageServices()),
+  ];
+
+  @override
+  final List<ModularRoute> routes = [
+    ModuleRoute(Modular.initialRoute, module: SplashModule()),
+    ModuleRoute('/home', module: HomeModule()),
+    ModuleRoute('/login', module: LoginModule()),
+  ];
+}
---
+++ lib/app/app_widget.dart
@@ -0,0 +1,14 @@
+import 'package:challenge_registration/app/core/core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+
+class AppWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Challenge Registration',
+      theme: AppTheme(context).defaultTheme,
+    ).modular();
+  }
+}
